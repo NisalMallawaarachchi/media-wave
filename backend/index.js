464e172bd9931230aa4d80e14b99b4e481b4c82d
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-28 18:10:25 +0530

    feat: configure environment variables and add MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,25 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load environment variables early
+dotenv.config();
+
+// Connect to the database
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("✅ Connected to the database");
+  })
+  .catch((error) => {
+    console.error("❌ Failed to connect to the database:", error);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000...');
+const PORT = process.env.PORT || 3000;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
