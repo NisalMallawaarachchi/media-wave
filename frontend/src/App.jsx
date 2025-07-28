34797faa65d29ff6828a05dbb0fcf90633d9d8eb
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-29 00:12:10 +0530

    feat: add UploadPage component for file upload with drag-and-drop support and progress indication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "../components/Header";
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import MediaGallery from "./pages/MediaGallery";
+import UploadPage from "./pages/UploadPage";
 
 export default function App() {
+
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/media-gallery" element={<MediaGallery />} />
+        <Route path="/upload" element={<UploadPage />} />
       </Routes>
     </BrowserRouter>
   );
