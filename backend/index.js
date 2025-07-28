ef73805d6c3259c3052fb3c5b06288e61590c849
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-28 18:43:30 +0530

    feat: implement MongoDB connection logic in separate db.js file

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import connectDB from "./db.js";
 
 // Load environment variables early
 dotenv.config();
 
 // Connect to the database
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("✅ Connected to the database");
-  })
-  .catch((error) => {
-    console.error("❌ Failed to connect to the database:", error);
-  });
+connectDB();
 
+// Initialize the Express application
 const app = express();
+
+// Routes
 
 const PORT = process.env.PORT || 3000;
 
