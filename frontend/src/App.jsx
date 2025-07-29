1ae472d844f95f97c809382a82dbedad31cac12a
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-29 13:53:15 +0530

    feat: add routes for GalleryPage and ContactPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import MediaGallery from "./pages/MediaGallery";
 import UploadPage from "./pages/UploadPage";
+import GalleryPage from "./pages/GalleryPage";
+import ContactPage from "./pages/ContactPage";
 
 export default function App() {
 
@@ -18,6 +20,8 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/media-gallery" element={<MediaGallery />} />
         <Route path="/upload" element={<UploadPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   );
