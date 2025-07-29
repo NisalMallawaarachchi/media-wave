4b700cd379b6f5672e02a06d160668937efaec0e
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-07-29 13:47:39 +0530

    fear: create upload image routes

--- backend/routes/upload.route.js
+++ backend/routes/upload.route.js
@@ -1,12 +1,13 @@
-// routes/uploadRoutes.js
 import express from 'express';
 import multer from 'multer';
-import { uploadFiles, deleteFile } from '../controllers/upload.controller.js';
+import { uploadFiles, deleteFile, getMediaList, getMediaDetails } from '../controllers/upload.controller.js';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/upload', upload.array('files'), uploadFiles);
+router.get('/all', getMediaList);
+router.get('/:id', getMediaDetails); // GET /api/media/:id
 router.delete('/delete/:public_id', deleteFile);
 
 export default router;
